1051aad0de2c5a4c055bb3a88b925e5f409aa137
Deftu <deftudev@gmail.com>
2023-04-30 15:22:17 +0200

    Update DGT - Part 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,10 @@ plugins {
 }
 
 preprocess {
+    val fabric11903 = createNode("1.19.3-fabric", 11903, "yarn")
     val fabric11902 = createNode("1.19.2-fabric", 11902, "yarn")
     val fabric11802 = createNode("1.18.2-fabric", 11802, "yarn")
 
+    fabric11903.link(fabric11902)
     fabric11902.link(fabric11802)
 }
