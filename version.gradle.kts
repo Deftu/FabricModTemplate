ba33b7fd4158c62c5e1b3a48a8bfa3a8f1a4f708
Deftu <deftudev@gmail.com>
2023-04-30 15:26:19 +0200

    Update DGT - Part 3

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,11 +1,9 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     java
     kotlin("jvm")
     id("xyz.deftu.gradle.multiversion")
     id("xyz.deftu.gradle.tools")
-    id("xyz.deftu.gradle.tools.loom")
+    id("xyz.deftu.gradle.tools.minecraft.loom")
 }
 
 repositories {
@@ -17,16 +15,18 @@ dependencies {
     implementation(kotlin("stdlib"))
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:${when (mcData.version) {
-        11902 -> "0.66.0+1.19.2"
-        11802 -> "0.66.0+1.18.2"
+        11903 -> "0.76.1+1.19.3"
+        11902 -> "0.76.0+1.19.2"
+        11802 -> "0.76.0+1.18.2"
         else -> throw IllegalStateException("Invalid MC version: ${mcData.version}")
     }}")
 
     modImplementation("net.fabricmc:fabric-language-kotlin:1.8.6+kotlin.1.7.21")
 
     modImplementation("com.terraformersmc:modmenu:${when (mcData.version) {
-        11902 -> "4.0.6"
-        11802 -> "3.2.3"
+        11903 -> "5.0.2"
+        11902 -> "4.1.2"
+        11802 -> "3.2.5"
         else -> throw IllegalStateException("Invalid MC version: ${mcData.version}")
     }}")
 }
